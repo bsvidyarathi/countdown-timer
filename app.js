f28b08495d33e93c038959dc5c457cc2073a1e4d
bsvidyarathi <71448085+bsvidyarathi@users.noreply.github.com>
2020-09-30 13:08:10 +1000

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ let tempYear = tempDate.getFullYear();
 let tempMonth = tempDate.getMonth();
 let tempDay = tempDate.getDate();
 // months are ZERO index based;
-const futureDate = new Date(tempYear, tempMonth, tempDay+1 , 11, 30, 0);
+const futureDate = new Date(tempYear, tempMonth, tempDay+2 , 11, 30, 0);
 
 // let futureDate = new Date(2020, 9, 24, 11, 30, 0);
 
